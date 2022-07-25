545a5545078a6763ddd34ae08c749c2b4245de98
IamGab <gabrielarafol.mendoza@tup.edu.ph>
2022-07-25 21:38:10 +0800

    refactor(routes): :rocket: add routes for services

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,13 +12,22 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource("/service", ServiceController::class);
+Route::get("/service/restore/{id}", [
+    "uses" => "ServiceController@restore",
+    "as" => "service.restore",
+]);
+Route::get("/service/forceDelete/{id}", [
+    "uses" => "ServiceController@forceDelete",
+    "as" => "service.forceDelete",
+]);
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
