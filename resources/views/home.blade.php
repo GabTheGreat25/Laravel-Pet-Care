d67f432244f609bdb94504f54d52c75a05d51bef
IamGab <gabrielarafol.mendoza@tup.edu.ph>
2022-07-29 23:49:44 +0800

    refactor(blade): :recycle: update navbar design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,56 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>@yield('title')</title>
-        <!-- Latest compiled and minified CSS -->
-          <!-- change link nalang -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
-        <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-        <link rel="stylesheet" href="{{ url('src/css/app.css')}}">
-        @yield('styles')
-    </head>
-    <body style="background-image: url(homepagebg.png); background-size:cover;">
-        @include('partials.header')
-        <div class="container">
+
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <!-- Latest compiled and minified CSS -->
+    <!-- change link nalang -->
+    <style>
+        html,
+        body {
+            height: 100%;
+            width: 100%;
+        }
+
+        main {
+            height: 100%;
+            width: 100%;
+            background-image: url(homepagebg.png);
+            background-size: cover;
+            background-repeat: no-repeat;
+            display: grid;
+            grid-template-rows: 5rem auto;
+
+
+        }
+    </style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ url('src/css/app.css') }}">
+    @yield('styles')
+</head>
+
+<main>
+    <div class="test">
+        <div>
+            @include('partials.header')
+        </div>
+        <div>
             @yield('content')
         </div>
-        <script src="https://cdnjs.cloudflare.com/.../Chart.js/2.9.3/Chart.min.js"></script>
-        <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-        <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/.../js/bootstrap.min.js" ></script>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-        @yield('scripts')
-        </body>
-    </html>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/.../Chart.js/2.9.3/Chart.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/.../js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+    @yield('scripts')
+</main>
+
+</html>
