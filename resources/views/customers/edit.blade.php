1e044cdf17e06e5202b4f1504e715f8994a81f0b
IamGab <gabrielarafol.mendoza@tup.edu.ph>
2022-08-03 19:20:35 +0800

    feat(blade): :recycle: add css design in edit

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -1,111 +1,124 @@
 @extends('html.usermaster')
 @section('title')
-    Customer Edit
+Customer Edit
 @endsection
 @section('contents')
+<div>
     <div>
-        <div>
-            <h1>
-                Update customers
-            </h1>
-        </div>
-        <div>
-            <div>
+        <h1 style="text-align:
+        center; font-weight: 700;">
+            Update Customers
+        </h1>
+    </div>
+    <div class="modal-body mx-3" style="display: grid; justify-content:center;">
+        <div class="md-form mb-5">
+            <div style="display: inline-block; width: 40rem;">
                 {{ Form::model($customers, [
-                    'route' => ['customer.update', $customers->id],
-                    'method' => 'PUT',
-                    'enctype' => 'multipart/form-data',
+                'route' => ['customer.update', $customers->id],
+                'method' => 'PUT',
+                'enctype' => 'multipart/form-data',
                 ]) }}
                 <div>
-
                     <div>
-                        <label for="title">Title</label>
+                        <label for="title">
+                            Title</label>
                         {{ Form::text('title', null, [
-                            'id' => 'title',
+                        'id' => 'title',
+                        'class' => 'form-control',
                         ]) }}
                         @if ($errors->has('title'))
-                            <p>{{ $errors->first('title') }}</p>
+                        <p>{{ $errors->first('title') }}</p>
                         @endif
                     </div>
-
+                    <br>
                     <div>
                         <div>
                             <label for="firstName">First Name</label>
                             {{ Form::text('firstName', null, [
-                                'id' => 'firstName',
+                            'id' => 'firstName',
+                            'class' => 'form-control',
                             ]) }}
                             @if ($errors->has('firstName'))
-                                <p>{{ $errors->first('firstName') }}</p>
+                            <p>{{ $errors->first('firstName') }}</p>
                             @endif
                         </div>
-
+                        <br>
                         <div>
                             <div>
                                 <label for="lastName">Last Name</label>
                                 {{ Form::text('lastName', null, [
-                                    'id' => 'lastName',
+                                'id' => 'lastName',
+                                'class' => 'form-control',
                                 ]) }}
                                 @if ($errors->has('lastName'))
-                                    <p>{{ $errors->first('lastName') }}</p>
+                                <p>{{ $errors->first('lastName') }}</p>
                                 @endif
                             </div>
-
-
+                            <br>
                             <div>
                                 <div>
                                     <label for="age">Age</label>
                                     {{ Form::text('age', null, [
-                                        'id' => 'age',
+                                    'id' => 'age',
+                                    'class' => 'form-control',
                                     ]) }}
                                     @if ($errors->has('age'))
-                                        <p>{{ $errors->first('age') }}</p>
+                                    <p>{{ $errors->first('age') }}</p>
                                     @endif
                                 </div>
-
+                                <br>
                                 <div>
                                     <label for="address">Address</label>
-                                    {{ Form::text('address', null, ['id' => 'address']) }}
+                                    {{ Form::text('address', null, ['id' => 'address', 'class' => 'form-control']) }}
                                     @if ($errors->has('address'))
-                                        <p>{{ $errors->first('address') }}</p>
+                                    <p>{{ $errors->first('address') }}</p>
                                     @endif
                                 </div>
-
+                                <br>
                                 <div>
                                     <label for="sex">Sex</label>
                                     {{ Form::text('sex', null, [
-                                        'id' => 'sex',
+                                    'id' => 'sex',
+                                    'class' => 'form-control',
                                     ]) }}
                                     @if ($errors->has('sex'))
-                                        <p>{{ $errors->first('sex') }}</p>
+                                    <p>{{ $errors->first('sex') }}</p>
                                     @endif
                                 </div>
-
+                                <br>
                                 <div>
                                     <label for="phonenumber">Phone Number</label>
-                                    {{ Form::text('phonenumber', null, ['id' => 'phonenumber']) }}
+                                    {{ Form::text('phonenumber', null, ['id' => 'phonenumber', 'class' =>
+                                    'form-control']) }}
                                     @if ($errors->has('phonenumber'))
-                                        <p>{{ $errors->first('phonenumber') }}</p>
+                                    <p>{{ $errors->first('phonenumber') }}</p>
                                     @endif
+                                    <br>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <label data-error="wrong" data-success="right" for="img_path"
-                style="display: inline-block;
+        </div>
+        <label data-error="wrong" data-success="right" for="img_path" style="display: inline-block;
           width: 150px; ">Customer Image</label>
-            <input type="file" id="img_path" class="form-control validate" name="img_path">
+        <input type="file" id="img_path" class="form-control validate" name="img_path">
 
-            <div>
-                <button type="submit">
+        <div>
+            <br>
+            <div style="display: grid; grid-template-columns: .5fr .5fr; gap: 2rem; ">
+                <button type="submit" class="btn btn-success">
                     Submit
                 </button>
-                <a href="{{ url()->previous() }}" role="button">Cancel</a>
+                <div style="display: grid; justify-content: center;" class="btn btn-danger">
+                    <a href="{{ url()->previous() }}" role="button">Cancel</a>
+                </div>
             </div>
         </div>
-        </form>
     </div>
-    </div>
-    </div>
+    </form>
+</div>
+</div>
+</div>
 @endsection
--- resources/views/services/edit.blade.php
+++ resources/views/services/edit.blade.php
@@ -1,64 +1,69 @@
 @extends('html.usermaster')
 @section('title')
-    Service Edit
+Service Edit
 @endsection
 @section('contents')
+<div>
     <div>
-        <div>
-            <h1>
-                Update Services
-            </h1>
-        </div>
-        <div>
-            <div>
+        <h1 style="text-align:
+                center; font-weight: 700;">
+            Update Services
+        </h1>
+    </div>
+    <div class="modal-body mx-3" style="display: grid; justify-content:center;">
+        <div class="md-form mb-5">
+            <div style="display: inline-block; width: 40rem;">
                 {{ Form::model($services, [
-                    'route' => ['service.update', $services->id],
-                    'method' => 'PUT',
-                    'enctype' => 'multipart/form-data',
+                'route' => ['service.update', $services->id],
+                'method' => 'PUT',
+                'enctype' => 'multipart/form-data',
                 ]) }}
                 <div>
                     <div>
                         <label for="servname">Service Name</label>
                         {{ Form::text('servname', null, [
-                            'id' => 'servname',
+                        'id' => 'servname',
+                        'class' => 'form-control',
                         ]) }}
                         @if ($errors->has('servname'))
-                            <p>{{ $errors->first('servname') }}</p>
+                        <p>{{ $errors->first('servname') }}</p>
                         @endif
                     </div>
 
                     <div>
                         <label for="description">Description</label>
                         {{ Form::text('description', null, [
-                            'id' => 'description',
+                        'id' => 'description',
+                        'class' => 'form-control',
                         ]) }}
                         @if ($errors->has('description'))
-                            <p>{{ $errors->first('description') }}</p>
+                        <p>{{ $errors->first('description') }}</p>
                         @endif
                     </div>
 
                     <div>
                         <label for="price">Price</label>
-                        {{ Form::text('price', null, ['id' => 'price']) }}
+                        {{ Form::text('price', null, ['id' => 'price', 'class' => 'form-control',]) }}
                         @if ($errors->has('price'))
-                            <p>{{ $errors->first('price') }}</p>
+                        <p>{{ $errors->first('price') }}</p>
                         @endif
                     </div>
-
-                    <label data-error="wrong" data-success="right" for="image"
-                        style="display: inline-block;
-          width: 150px; ">Service Image</label>
-                    <input type="file" id="image" class="form-control validate" name="image">
-
-                    <div>
-                        <button type="submit">
-                            Submit
-                        </button>
-                        <a href="{{ url()->previous() }}" role="button">Cancel</a>
-                    </div>
                 </div>
-                </form>
             </div>
         </div>
+        <label data-error="wrong" data-success="right" for="img_path" style="display: inline-block;
+          width: 150px; ">Service Image</label>
+        <input type="file" id="img_path" class="form-control validate" name="img_path">
+
+        <div>
+            <button type="submit">
+                Submit
+            </button>
+            <a href="{{ url()->previous() }}" role="button">Cancel</a>
+
+        </div>
+        </form>
     </div>
+</div>
+</div>
 @endsection
