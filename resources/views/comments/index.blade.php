8e3c95840f915f499ce683756071836576b48970
IamGab <gabrielarafol.mendoza@tup.edu.ph>
2022-08-10 20:45:11 +0800

    refactor(blade): :recycle: fix comment ui

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -5,188 +5,98 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <style>
-    body{
-        margin-top:20px;
-        background-color:#e9ebee;
+    body {
+        margin-top: 3rem;
+        background-color: #e9ebee;
     }
-    
+
     .be-comment-block {
-        margin-bottom: 50px !important;
-        border: 1px solid #edeff2;
         border-radius: 2px;
-        padding: 50px 70px;
-        border:1px solid #ffffff;
+        padding: 2rem 70px 1rem 70px;
+        background-color: #ffffff;
     }
-    
+
     .comments-title {
         font-size: 16px;
         color: #262626;
         margin-bottom: 15px;
         font-family: 'Conv_helveticaneuecyr-bold';
     }
-    
-    .be-img-comment {
-        width: 60px;
-        height: 60px;
-        float: left;
-        margin-bottom: 15px;
-    }
-    
-    .be-ava-comment {
-        width: 60px;
-        height: 60px;
-        border-radius: 50%;
-    }
-    
+
+
     .be-comment-content {
-        margin-left: 80px;
+        border: 2.5px solid #ced2d7d6;
+        padding: 1rem 2rem;
+        margin: 2rem 0;
     }
-    
+
     .be-comment-content span {
         display: inline-block;
         width: 49%;
         margin-bottom: 15px;
     }
-    
+
+
     .be-comment-name {
-        font-size: 13px;
+        font-size: 1rem;
+        font-weight: 500;
         font-family: 'Conv_helveticaneuecyr-bold';
     }
-    
-    .be-comment-content a {
-        color: #383b43;
-    }
-    
-    .be-comment-content span {
-        display: inline-block;
-        width: 49%;
-        margin-bottom: 15px;
-    }
-    
+
     .be-comment-time {
         text-align: right;
-    }
-    
-    .be-comment-time {
-        font-size: 11px;
+        font-size: 1rem;
+        font-weight: 700;
         color: #b4b7c1;
     }
-    
+
     .be-comment-text {
-        font-size: 13px;
-        line-height: 18px;
-        color: #7a8192;
+        font-size: 1.15rem;
+        font-weight: 500;
+        color: black;
         display: block;
         background: #f6f6f7;
         border: 1px solid #edeff2;
         padding: 15px 20px 20px 20px;
     }
-    
-    .form-group.fl_icon .icon {
-        position: absolute;
-        top: 1px;
-        left: 16px;
-        width: 48px;
-        height: 48px;
-        background: #f6f6f7;
-        color: #b5b8c2;
-        text-align: center;
-        line-height: 50px;
-        -webkit-border-top-left-radius: 2px;
-        -webkit-border-bottom-left-radius: 2px;
-        -moz-border-radius-topleft: 2px;
-        -moz-border-radius-bottomleft: 2px;
-        border-top-left-radius: 2px;
-        border-bottom-left-radius: 2px;
-    }
-    
-    .form-group .form-input {
-        font-size: 13px;
-        line-height: 50px;
-        font-weight: 400;
-        color: #b4b7c1;
-        width: 100%;
-        height: 50px;
-        padding-left: 20px;
-        padding-right: 20px;
-        border: 1px solid #edeff2;
-        border-radius: 3px;
-    }
-    
-    .form-group.fl_icon .form-input {
-        padding-left: 70px;
-    }
-    
-    .form-group textarea.form-input {
-        height: 150px;
-    }
-        </style>
+</style>
 
-    <body>
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-    </body> 
-
-        <div class="container">
+<body>
+    <div class="container">
         <div class="be-comment-block">
 
-    <h1 class="comments-title"><i class="fa fa-comments" aria-hidden="true"></i>Comments</h1>
+            <h1 class="comments-title"><i class="fa fa-comments" aria-hidden="true"></i>Comments</h1>
 
-    <h3><a href="{{ route('comments.create') }}" class="btn btn-success" role="button">ADD COMMENT</a></h3>
-          
-	<div class="be-comment-content">
+            <h3><a href="{{ route('comments.create') }}" class="btn btn-success" role="button">ADD COMMENT</a></h3>
 
-        @forelse($services as $comment)
-        <span class="be-comment-name">
-            <a>{{$comment->guestName}} | {{$comment->gEmail}}</a>
-            </span>
+            <div class="be-comment-content">
 
-        <span class="be-comment-time">
-            <i class="fa fa-clock-o"></i>
-            {{$comment->created_at}}
-        </span>
+                @forelse($services as $comment)
+                <span class="be-comment-name">
+                    <a>{{$comment->guestName}} | {{$comment->gEmail}}</a>
+                </span>
 
-    <p class="be-comment-text">
-        {{$comment->gcomment}}
-    </p>
+                <span class="be-comment-time">
+                    <i class="fa fa-clock-o"></i>
+                    {{$comment->created_at}}
+                </span>
 
-
-
-
-{{-- <h1 class="comments-title">Comments (3)</h1>
-
-	<div class="be-comment">
-		<div class="be-img-comment">	
-			<a href="blog-detail-2.html">
-				<img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" class="be-ava-comment">
-			</a>
-		</div>
-
-
-		<div class="be-comment-content">
-			
-				<span class="be-comment-name">
-					<a href="blog-detail-2.html">Ravi Sah</a>
-					</span>
-				<span class="be-comment-time">
-					<i class="fa fa-clock-o"></i>
-					May 27, 2015 at 3:14am
-				</span>
-
-			<p class="be-comment-text">
-				Pellentesque gravida tristique ultrices. 
-				Sed blandit varius mauris, vel volutpat urna hendrerit id. 
-				Curabitur rutrum dolor gravida turpis tristique efficitur.
-			</p>
-		</div>
-	</div>
-	 --}}
-     @empty
-     <h2 class="text-center text-4xl py-8">NO COMMENTS</h2>
-     @endforelse
-
-	</div>
-</div>
-</div>
-</div>
+                <p class="be-comment-text">
+                    {{$comment->gcomment}}
+                </p>
+                @empty
+                <h2 class="text-center text-4xl py-8">NO COMMENTS</h2>
+                @endforelse
+            </div>
+            <div style="display: grid; justify-content: end;">
+                <a href="/data" class="btn btn-danger"
+                    style="padding: .7rem 1.5rem; font-size: 1rem; font-weight: 500; font-style:italic;"
+                    role="button">Go
+                    Back</a>
+            </div>
+        </div>
+    </div>
+</body>
